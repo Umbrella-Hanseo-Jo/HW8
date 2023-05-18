1d25ed62743da918a02fe1cf28282e3608cad69d
unknown <0214gxqs@gmail.com>
2023-05-18 22:34:57 +0900

    v1

---
+++ HW8.c
@@ -0,0 +1,206 @@
+/*
+ *  doubly circular linked list
+ *
+ *  Data Structures
+ *
+ *  School of Computer Science
+ *  at Chungbuk National University
+ *
+ */
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+	int key;
+	struct Node* llink;
+	struct Node* rlink;
+} listNode;
+
+int initialize(listNode** h);
+int freeList(listNode* h);
+int insertLast(listNode* h, int key);
+int deleteLast(listNode* h);
+int insertFirst(listNode* h, int key);
+int deleteFirst(listNode* h);
+int invertList(listNode* h);
+int insertNode(listNode* h, int key);
+int deleteNode(listNode* h, int key);
+void printList(listNode* h);
+
+int main()
+{
+
+   printf("[----- [조한서] [2022041012] -----]");
+
+
+	char command;
+	int key;
+	listNode* headnode=NULL;
+
+	do{
+		printf("----------------------------------------------------------------\n");
+		printf("                  Doubly Circular Linked List                   \n");
+		printf("----------------------------------------------------------------\n");
+		printf(" Initialize    = z           Print         = p \n");
+		printf(" Insert Node   = i           Delete Node   = d \n");
+		printf(" Insert Last   = n           Delete Last   = e\n");
+		printf(" Insert First  = f           Delete First  = t\n");
+		printf(" Invert List   = r           Quit          = q\n");
+		printf("----------------------------------------------------------------\n");
+
+		printf("Command = ");
+		scanf(" %c", &command);
+
+		switch(command) {
+		case 'z': case 'Z':
+			initialize(&headnode);
+			break;
+		case 'p': case 'P':
+			printList(headnode);
+			break;
+		case 'i': case 'I':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertNode(headnode, key);
+			break;
+		case 'd': case 'D':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			deleteNode(headnode, key);
+			break;
+		case 'n': case 'N':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertLast(headnode, key);
+			break;
+		case 'e': case 'E':
+			deleteLast(headnode);
+			break;
+		case 'f': case 'F':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertFirst(headnode, key);
+			break;
+		case 't': case 'T':
+			deleteFirst(headnode);
+			break;
+		case 'r': case 'R':
+			invertList(headnode);
+			break;
+		case 'q': case 'Q':
+			freeList(headnode);
+			break;
+		default:
+			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
+			break;
+		}
+
+	}while(command != 'q' && command != 'Q');
+
+	return 1;
+}
+
+// 이중 원형 연결 리스트를 초기화 (헤드 노드를 생성 및 초기 설정 수행)
+int initialize(listNode** h) {
+
+	if(*h != NULL)
+		freeList(*h);
+	*h = (listNode*)malloc(sizeof(listNode));
+	(*h)->rlink = *h;
+	(*h)->llink = *h;
+	(*h)->key = -9999;
+	return 1;
+}
+
+// 리스트의 모든 노드를 메모리에서 해제
+int freeList(listNode* h){
+
+	return 0;
+}
+
+// 리스트의 노드들을 순차적으로 출력
+void printList(listNode* h) {
+	int i = 0;
+	listNode* p;
+
+	printf("\n---PRINT\n");
+
+	if(h == NULL) {
+		printf("Nothing to print....\n");
+		return;
+	}
+
+	p = h->rlink;
+
+	while(p != NULL && p != h) {
+		printf("[ [%d]=%d ] ", i, p->key);
+		p = p->rlink;
+		i++;
+	}
+	printf("  items = %d\n", i);
+
+	printf("\n---checking addresses of links\n");
+	printf("-------------------------------\n");
+	printf("head node: [llink]=%p, [head]=%p, [rlink]=%p\n", h->llink, h, h->rlink);
+
+	i = 0;
+	p = h->rlink;
+	while(p != NULL && p != h) {
+		printf("[ [%d]=%d ] [llink]=%p, [node]=%p, [rlink]=%p\n", i, p->key, p->llink, p, p->rlink);
+		p = p->rlink;
+		i++;
+	}
+
+}
+
+// 리스트의 마지막에 새로운 노드 삽입
+int insertLast(listNode* h, int key) {
+
+	return 1;
+}
+
+// 리스트의 마지막 노드 삭제
+int deleteLast(listNode* h) {
+
+
+	return 1;
+}
+
+// 리스트의 첫 번째에 새로운 노드를 삽입
+int insertFirst(listNode* h, int key) {
+
+
+	return 1;
+}
+
+// 리스트의 첫 번째 노드를 삭제
+int deleteFirst(listNode* h) {
+
+`
+	return 1;
+
+}
+
+
+// 리스트의 순서를 역순으로 뒤집는다.
+int invertList(listNode* h) {
+
+`
+	return 0;
+}
+
+// 지정한 키 값을 가지는 노드를 리스트에 삽입
+int insertNode(listNode* h, int key) {
+
+	return 0;
+}
+
+
+// 지정한 키 값을 가지는 노드를 리스트에서 삭제
+int deleteNode(listNode* h, int key) {
+
+	return 0;
+}
